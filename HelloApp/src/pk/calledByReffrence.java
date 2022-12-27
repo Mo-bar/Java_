b578d7f7ce23ec8d3165b5945137edc9236842b5
mourad_br <mouradbarkouch@gmail.com>
2022-12-27 23:36:07 +0100

    Appel par refference 'Call by reference'.

---
+++ HelloApp/src/pk/calledByReffrence.java
@@ -0,0 +1,20 @@
+package pk;
+
+/*
+ * Dans Call by value, une copie de la variable est passée alors que dans Call by reference,
+ *  une variable elle-même est passée. Dans Appel par valeur, 
+ * les arguments réels et formels seront créés dans différents
+ *  emplacements de mémoire tandis que dans Appel par référence, 
+ * les arguments réels et formels seront créés dans le même emplacement mémoire.
+ */
+public class calledByReffrence {
+    static void setArray(int[] x){
+        x[0] = 10;
+    }
+
+    public static void main(String[] args){
+        int ary[] = {1,2,3};
+        setArray(ary);
+        System.out.println(ary[0]);
+    }
+}
