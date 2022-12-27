6c2a2a45a65a7bbbff6a5a774e94b37af380a4b5
mourad_br <mouradbarkouch@gmail.com>
2022-12-27 22:17:28 +0100

    Overloading

---
+++ HelloApp/src/pk/Overloading.java
@@ -0,0 +1,14 @@
+package pk;
+
+public class Overloading{
+    static int sum(int n1, int n2) {
+        return n1+n2;
+    }
+    static float sum(float n1, float n2){
+        return n1 +n2;
+    }
+    static float sum(float n1, float n2,float n3){
+        return n1 +n2+ n3;
+    }
+
+}
