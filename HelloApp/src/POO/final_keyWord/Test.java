5d463484281a0e55a9d43e2d3096709b53b6a63e
mourad_br <mouradbarkouch@gmail.com>
2023-01-10 22:38:34 +0100

    Final key-word properties.

---
+++ HelloApp/src/POO/final_keyWord/Test.java
@@ -0,0 +1,22 @@
+package POO.final_keyWord;
+
+public class Test {
+    final int FINAL_VAR = 0;
+    final String FINAL_VAR1;
+    final String FINAL_VAL2;
+    /**
+     * ? Final variabble
+     * Le mot-cle Final en java est une modificateur utilise pour empeche la modification d'un code ou d'une valeur.
+     * Une fois initialise  la variable declare, ne peut etre pas la change
+     * 
+     * ? Final Methode : 
+     * 
+     */
+    {
+        FINAL_VAR1 = "";
+    }
+
+    Test(){
+        FINAL_VAL2 = "";
+    }
+}
